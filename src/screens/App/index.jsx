7d43d78bb88c2493fe0058eec795ab0af2be4f11
Alan Costa Facchini <costafacchini@gmail.com>
2024-06-08 20:02:15 -0300

    Create table to render stock report

--- src/screens/App/index.jsx
+++ src/screens/App/index.jsx
@@ -3,29 +3,62 @@ import './index.css'
 
 function App() {
   const [searchStockTicker, setSearchStockTicker] = useState('')
+  const [data, setData] = useState([])
 
   const handleInputChange = (event) => {
     setSearchStockTicker(event.target.value.toUpperCase())
   }
 
+  const fetchStockTickerReport = async () => {
+    const response = await window.fetch(`http://127.0.0.1:3000/v1/stock_report/${searchStockTicker}`)
+    return await response.json()
+  }
+
   const handleSearch = () => {
-    // fetch to get report data on API
-    console.log(searchStockTicker)
+    if (searchStockTicker !== '') {
+      fetchStockTickerReport().then(data => {
+        setData(data.report)
+      })
+    }
   }
 
   return (
-    <div className="container">
-      <div>
-        <label>Enter a Stock Ticker</label>
-        <input
-          type="text"
-          value={searchStockTicker}
-          onChange={handleInputChange}
-          placeholder="Digite sua pesquisa..."
-          />
-        <button onClick={handleSearch}>Search</button>
+    <>
+      <div className="app-container">
+        <div>
+          <label>Enter a Stock Ticker</label>
+          <input
+            type="text"
+            value={searchStockTicker}
+            onChange={handleInputChange}
+            placeholder="Enter Ticker Symbol..."
+            />
+          <button onClick={handleSearch}>Search</button>
+        </div>
       </div>
-    </div>
+      <div className="report-container">
+        <table>
+          <thead>
+            <tr>
+              <th className="report-container-header report-container-cell">Item</th>
+              <th className="report-container-header report-container-cell">Minimum</th>
+              <th className="report-container-header report-container-cell">Maximum</th>
+              <th className="report-container-header report-container-cell">Average</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data && data.map((item, index) => (
+              <tr key={index}>
+                <td className="report-container-cell">{item.label}</td>
+                <td className="report-container-cell">{item.maximum}</td>
+                <td className="report-container-cell">{item.minimum}</td>
+                <td className="report-container-cell">{item.average}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
